2abdfdb56632c6eeed47474c44737f5abeb18af9
shahriyor-sharifjonov <shahrizodadeveloping@gmail.com>
2023-07-14 13:09:45 +0500

    initial

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,5 @@
 import * as functions from "./modules/functions.js";
+import $  from 'jquery'
 
 functions.isWebp();
 
@@ -45,3 +46,44 @@ menu.addEventListener('click', (e)=>{
     list.classList.toggle('active')
     body.classList.toggle('active')
 })
+
+
+
+
+if(window.innerWidth >= 577) {
+    const btns = document.querySelectorAll('.guests__button');
+    const items = document.querySelectorAll('.guests__item');
+    
+    btns.forEach(btn=>{
+        btn.addEventListener('click', (e)=>{
+            btns.forEach(btn=>{
+                btn.classList.remove('active')
+            })
+            btn.classList.toggle('active')
+            items.forEach(item=>{
+                item.classList.remove('active')
+            })
+            const target = btn.getAttribute('data-target')
+            const el = document.querySelector(`${target}`);
+            el.classList.toggle('active')
+        })
+    })
+}else{
+    $(document).ready(function() {
+        $(".accordion > .accordion__button").on("click", function() {
+          if ($(this).hasClass("active")) {
+            $(this).removeClass("active");
+            $(this)
+              .siblings(".accordion__content")
+              .slideUp(200);
+          } else {
+            $(".accordion > .accordion__button").removeClass("active");
+            $(this).addClass("active");
+            $(".accordion__content").slideUp(200);
+            $(this)
+              .siblings(".accordion__content")
+              .slideDown(200);
+          }
+        });
+      });
+}
